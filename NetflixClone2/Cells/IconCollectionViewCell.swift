bb07a4a2dc1c2209ea5a1788f2a7edd89c1b7488
Rümeysa Tokur <rtokur11@gmail.com>
2025-02-10 20:04:49 +0300

    Revision is maded and profile section is done.

---
+++ NetflixClone2/Cells/IconCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  IconCollectionViewCell.swift
+//  NetflixClone2
+//
+//  Created by Rumeysa Tokur on 10.02.2025.
+//
+
+import UIKit
+
+class IconCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ NetflixClone2/Controllers/IconVC.swift
@@ -0,0 +1,29 @@
+//
+//  IconVC.swift
+//  NetflixClone2
+//
+//  Created by Rumeysa Tokur on 10.02.2025.
+//
+
+import UIKit
+
+class IconVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
